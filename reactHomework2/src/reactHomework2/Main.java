1384a5412d5d04b7beb9670dd48534503be62d55
dgnbe <dgnbe@192.168.88.2>
2021-04-30 15:07:20 +0300

    Main denemesi

---
+++ reactHomework2/src/reactHomework2/Main.java
@@ -0,0 +1,14 @@
+package reactHomework2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Deneme yapýyoruz");
+		
+		
+	}
+
+}
